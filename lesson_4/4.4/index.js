d354b9bd4691d17d8e0c2e63b64ef168cccac7a1
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2024-05-03 21:08:37 +0300

    change lesson 4.4

--- lesson_4/4.4/index.js
+++ lesson_4/4.4/index.js
@@ -1,13 +1,15 @@
 let numOrStr = prompt('input number or string');
-console.log(numOrStr);
 
-switch (numOrStr.trim()) {
-    case null:
+switch (true) {
+    case numOrStr === null:
         console.log('ви скасували');
         break;
-    case '':
+    case numOrStr.trim() === '':
         console.log('Empty String');
         break;
+    case isNaN( +numOrStr ):
+        console.log(' number is Ba_NaN');
+        break;
     default:
-        isNaN( +numOrStr ) ? console.log('number is Ba_NaN') : console.log('OK!');
+        console.log('OK!');
 }
