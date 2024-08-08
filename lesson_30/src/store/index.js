838d58c49a18d2e09217249771b0b7a833f0e3de
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2024-08-08 16:34:52 +0300

    complete lesson 30

--- lesson_30/src/store/index.js
+++ lesson_30/src/store/index.js
@@ -1,24 +1,30 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-import { useSelector } from 'react-redux';
+import {thunk} from 'redux-thunk'
+import { createStore, applyMiddleware  } from 'redux';
 
-const initialState = { list: [] };
+const rootReducer = (state = {data: null}, action) => {
+  switch (action.type) {
+    case 'GET':
+      return {data: action.payload};
 
-export const useTodos = () => useSelector(state => state.todos);
-
-const todoSlice = createSlice({
-  name: 'todo',
-  initialState,
-  reducers: {
-    add(state, action) {
-     return ({list: [...state.list, action.payload]})
-    }
-  },
-});
-
-export const { add } = todoSlice.actions;
-
-export const store = configureStore({ 
-  reducer: {
-    todos: todoSlice.reducer
+      case 'RESET':
+      return {data: null};
+    default:
+      return state;
   }
-});
+};
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export function fetchByEndpoint(endpoint) {
+  return async function fetchByEndpointThunk(dispatch) {
+    try {
+      const response = await fetch(`https://swapi.dev/api/${endpoint}`);
+      const data = await response.json();
+      
+      dispatch(({type: 'GET', payload: data}))
+    } catch(err) {
+      
+      console.log('something went wrong');
+    }
+  }
+}
