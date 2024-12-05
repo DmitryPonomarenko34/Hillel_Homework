f089f592a72f6be641dbeff40fc3aa4fb0f83367
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2024-12-05 06:25:27 +0200

    add react-router-dom

--- lesson_34/eslint.config.js
+++ lesson_34/eslint.config.js
@@ -18,6 +18,7 @@ export default [
       }
     },
     settings: { react: { version: '18.3' } },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
     plugins: {
       react,
       'react-hooks': reactHooks,
